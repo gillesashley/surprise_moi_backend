f46e9eb9b150de95c1b819a18f8487dde5d57a88
Gilles Ashley <xylaray37@gmail.com>
2026-02-28 07:25:25 +0000

    feat(conversations): implement online status for participants based on recent activity

--- app/Http/Resources/ConversationDetailResource.php
+++ app/Http/Resources/ConversationDetailResource.php
@@ -52,7 +52,7 @@ class ConversationDetailResource extends JsonResource
                 'name' => $otherParticipant->name,
                 'avatar' => $otherParticipant->avatar ? Storage::url($otherParticipant->avatar) : null,
                 'role' => $otherParticipant->role,
-                'is_online' => true,
+                'is_online' => $otherParticipant->isOnline(),
             ],
             'last_message' => $this->last_message,
             'last_message_at' => $this->last_message_at?->toIso8601String(),
