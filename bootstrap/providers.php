ff54fece973950298889904eeaa0cd91b5c66346
Gilles Ashley <xylaray37@gmail.com>
2026-03-14 08:45:59 +0000

    seting up telescope

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,13 @@
 <?php
 
-return [
+$providers = [
     App\Providers\AppServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\HorizonServiceProvider::class,
 ];
+
+if (class_exists(\Laravel\Telescope\TelescopeApplicationServiceProvider::class)) {
+    $providers[] = App\Providers\TelescopeServiceProvider::class;
+}
+
+return $providers;
